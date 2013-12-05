eb1dc43c0b03232bc45f88cdff0628d08c1da0a3
Vivek Tumrukota <vtumrukota@gmail.com>
2013-12-05 01:31:32 +0000

    Updtaed web.js

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response){
 var fs = require('fs');
 var data = fs.readFileSync("index.html", "utf-8");
-    response.send(data.toString('utf-8'));
+    response.send(data.toString());
 });
 
-var port = processB.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
