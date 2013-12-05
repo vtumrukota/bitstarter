fdb46ac5d2917e1a2f1d36ae2a164cbd526c5bb4
Vivek Tumrukota <vtumrukota@gmail.com>
2013-12-05 00:32:24 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
+var buffer = new Buffer(16);
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var input = fs.readFileSync("index.html", "utf-8");
+console.log(buffer.toString('utf-8',0,buffer.length(input));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
