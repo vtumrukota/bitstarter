b6e09249d08575ce488ff2fb3227379a59698069
Vivek Tumrukota <vtumrukota@gmail.com>
2013-12-05 00:51:52 +0000

    Updtaed web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
-var buffer = new Buffer(16);
+var fs = require('fs');
 app.use(express.logger());
 
 var input = fs.readFileSync("index.html", "utf-8");
-console.log(buffer.toString('utf-8',0,buffer.length(input));
 
-var port = process.env.PORT || 5000;
+app.get('/', function(request, response){
+    response.send(input);
+});
+
+var port = processB.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
